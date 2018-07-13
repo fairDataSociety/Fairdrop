c36cef2f33abb0a2a3fb3772b9d9f48c49e959d0
significance <dan@1up.digital>
2018-07-13 09:49:10 +0100

    dwallet component intial

--- src/components/DWallet.js
+++ src/components/DWallet.js
@@ -3,21 +3,47 @@ import React, { Component } from 'react';
 class DWallet extends Component {
   constructor(props) {
     super(props);
-    
     this.state = {    
-      walletIsSelected: false
+      walletIsSelected: false,
+      selectedWalletFileName: ""
+    }
+
+    this.handleSelectWallet = this.handleSelectWallet.bind(this);
+    this.handleFileChange = this.handleFileChange.bind(this);
+    this.handleUnlockWallet = this.handleUnlockWallet.bind(this);
+
+  }
+
+  handleSelectWallet(e){
+    e.preventDefault();    
+    this.refs.dtWalletFileInput.click();
+  }
+
+  handleFileChange(e){
+    e.preventDefault();
+    if(this.refs.dtWalletFileInput.files[0]){
+      this.setState({
+        walletIsSelected: true,
+        selectedWalletFileName: this.refs.dtWalletFileInput.files[0].name
+      });
     }
   }
 
-  selectWallet(){
-
-  }
-
+  handleUnlockWallet(e){
+    e.preventDefault();
+    console.log(this.refs.dtPasswordInput.value);
+  }  
 
   render() {
     return (
       <div>
-        <button className="" onClick={this.selectWallet} /><label></label>
+      <form>
+        <button id="dt-select-wallet-button" className="dt-toggle-button" onClick={this.handleSelectWallet} /><label>Select Your Wallet</label>
+        <p>{this.selectedWalletFileName}</p>
+        <input autoComplete="off" className="dt-hidden-file-input" type="file" onChange={this.handleFileChange} ref="dtWalletFileInput"/>
+        <input autoComplete="off" className="dt-password-input" type="password" ref="dtPasswordInput" />
+        <button className="dt-unlock-wallet-button" onClick={this.handleUnlockWallet} /><label>Unlock Your Wallet</label>
+      </form>
       </div>
     );
   }
--- src/components/DWallet.test.js
+++ src/components/DWallet.test.js
@@ -1,9 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DWallet from "./DWallet";
+import {shallow, mount} from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<DWallet />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('#dt-toggle-button', ()=>{
+    it('should fire toggleFileShouldEncrypt event',()=>{
+
+      const spy = jest.spyOn(DWallet.prototype, 'handleSelectWallet');
+
+      const component = mount(
+        <DWallet />,
+      );
+
+      component.update();
+
+      component.find('#dt-select-wallet-button').simulate('click');
+
+      expect(spy).toHaveBeenCalled();
+
+    });
+});
+
+describe('#dt-select-wallet-button', ()=>{
+    it('should fire toggleFileShouldEncrypt event',()=>{
+
+      const spy = jest.spyOn(DWallet.prototype, 'handleSelectWallet');
+
+      const component = mount(
+        <DWallet />,
+      );
+
+      component.update();
+
+      component.find('#dt-select-wallet-button').simulate('click');
+
+      expect(spy).toHaveBeenCalled();
+
+    });
 });

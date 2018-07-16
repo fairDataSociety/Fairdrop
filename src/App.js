e5b353cd1a8e0c4e003a3735ffe5eae741362c17
significance <dan@1up.digital>
2018-07-16 11:51:53 +0100

    dropzone intial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import DWallet from "./components/DWallet";
 import DTransfer from "./services/Dtransfer";
+import Dropzone from 'dropzone'
+
 import './App.css';
 
 class App extends Component {
@@ -12,12 +14,39 @@ class App extends Component {
     };
 
     this.handleSelectFileForUpload = this.handleSelectFileForUpload.bind(this);
+    this.fireSelectFile = this.fireSelectFile.bind(this);
 
   }
 
   handleSelectFileForUpload(e){
     e.preventDefault();    
     this.setState({fileIsSelecting: !this.state.fileIsSelecting});
+  }
+
+  onDrop(acceptedFiles, rejectedFiles) {
+    debugger
+  }
+
+  fireSelectFile(){
+    this.setState({fileIsSelecting: true});
+    setTimeout(()=>{
+      this.refs.dtSelectFile.click();
+    },1000);
+  }
+
+  componentDidMount(){
+    this.dropzone = new Dropzone(this.refs.dtSelectFile, { url: "/file/post"});
+    this.dropzone.on("dragenter", (event) => {
+     this.setState({fileIsSelecting: true});
+    }); 
+    this.dropzone.on("dragleave", (event) => {
+      if(event.fromElement === null){
+        this.setState({fileIsSelecting: false});
+      }
+    });    
+    this.dropzone.on("addedfile", (file) => {
+      console.log(file);
+    });
   }
 
   render() {
@@ -27,14 +56,14 @@ class App extends Component {
           <div className="dt-logo">
           </div>
         </div>
-        <div className="dt-select-file" onClick={this.handleSelectFileForUpload}> 
-          <div className={"dt-select-file-header " + (this.state.fileIsSelecting && "is-selected")}> {/* this bit should slide up out of view using transform */}
+        <div id="dt-select-file" className="dt-select-file" ref="dtSelectFile" > 
+          <div className={"dt-select-file-header " + (this.state.fileIsSelecting && "is-selecting")} onClick={this.fireSelectFile}> {/* this bit should slide up out of view using transform */}
             <h1>Send and store files securely and privately<br/>that's how we do on the decentralised web 3.0</h1>
           </div> {/* dt-header */}
-          <div className={"dt-select-file-main " + (this.state.fileIsSelecting && "is-selected")} > {/* this bit should expand to fill the viewport */}
+          <div className={"dt-select-file-main " + (this.state.fileIsSelecting && "is-selecting")} > {/* this bit should expand to fill the viewport */}
 
           </div> {/* dt-select-file-main */}
-          <div className={"dt-select-file-instruction " + (this.state.fileIsSelecting && "is-selected")}> {/* this bit should be centered vertically in the surrounding div which overlays the other two siblings */}
+          <div className={"dt-select-file-instruction " + (this.state.fileIsSelecting && "is-selecting")} onClick={this.fireSelectFile}> {/* this bit should be centered vertically in the surrounding div which overlays the other two siblings */}
             <h2>choose or drag and drop a file</h2>
           </div> {/* dt-select-file-instruction */}
         </div> {/* dt-select-file */}

7846e31e3ee65fb226cf0cf3c246ed58af7541ce
significance <dan@1up.digital>
2018-08-19 13:32:18 +0100

    adds progress bar

--- src/components/up/ProgressBar.js
+++ src/components/up/ProgressBar.js
@@ -8,8 +8,25 @@ class FCompleted extends Component{
 
   render(){
     return (
-      <div id="dt-progress" className={'ui-state-'+this.props.parentState.uiState}> 
-        progress
+      <div id='dt-progress' className={'dt-progress dt-ui-state-'+this.props.parentState.uiState}> 
+        <div class='dt-progress-indicator'></div>
+        <div class='dt-progress-labels'>
+          <div class='dt-progress-label'>
+            1 Select File
+          </div>
+          <div class='dt-progress-label'>
+            2 Encrypt
+          </div>
+          <div class='dt-progress-label'>
+            3 Add Recipients
+          </div>
+          <div class='dt-progress-label'>
+            4 Upload
+          </div>
+          <div class='dt-progress-label'>
+            5 Summary
+          </div>
+        </div>
       </div>
     )
   }

b41532779d51411fcc6266e3531743f3d9918556
significance <dan@1up.digital>
2018-10-28 17:02:58 +0100

    improves summary page

--- src/components/up/FCompleted.js
+++ src/components/up/FCompleted.js
@@ -5,42 +5,29 @@ class FCompleted extends Component{
 
   render(){
     return (
-      <div id="dt-completed" className={"dt-confirm dt-green dt-page-wrapper " + (this.props.parentState.uiState === 5 ? "dt-fade-in" : "dt-hidden")}> 
+      <div id="dt-completed" className={"dt-confirm dt-page-wrapper " + (this.props.parentState.uiState === 5 ? "dt-fade-in" : "dt-hidden")}> 
           <div className={"dt-info " + (this.props.parentState.fileIsSelected && "is-selected")}> {/* this bit slides in from left over the top of dt-select-file */}
             <div className="dt-info-content">
-              <img className="dt-file-icon" src="/assets/images/file-icon.svg" alt="File Icon"/>
-              <div className="dt-info-filename">{this.props.parentState.selectedFileName}</div>
-              <div className="dt-info-filesize">{ Utils.humanFileSize(this.props.parentState.selectedFileSize) }</div>
-              <div className="dt-info-is-encrypted">
-                <img className="dt-fairdrop-lock" src="/assets/images/fairdrop-lock.svg" alt="fairdrop-logo"/> File is encrypted
+              <div className="dt-file-was-sent">
+                <img className="dt-circle-tick" src="/assets/images/circle-tick.svg" alt="Circle Tick"/>
+                File is {this.props.parentState.isStoringFile === false ? 'sent.' : 'stored.'}
               </div>
-              {this.props.parentState.isStoringFile === true && 
-                <div>
-                  <div className="dt-feedback-message">File stored.</div>                  
-                </div>
-              }
-              {this.props.parentState.isStoringFile === false && 
-                <div>
-                  <div className="dt-feedback-message">from: { this.props.parentState.selectedMailbox.subdomain }</div>
-                  <div className="dt-feedback-message">to: { this.props.parentState.addressee }</div>  
-                </div>
-              }
-              <div className="dt-feedback-message">{this.props.parentState.feedBackMessage}</div>
-            </div>
-          </div> {/* dt-info */}
-          <div className={"dt-ui " + (this.props.parentState.fileIsSelected && "is-selected")}> {/* this bit slides in from right over the top of dt-select-file */}
+              <div className="dt-info-filename">
+                {this.props.parentState.selectedFileName}
+                <span className="dt-info-filesize"> { Utils.humanFileSize(this.props.parentState.selectedFileSize) }</span>
+              </div>
+              
+              <div className="dt-info-is-encrypted">
+                <img className="dt-fairdrop-lock" src="/assets/images/fairdrop-lock.svg" alt="fairdrop-logo"/> Encrypted
+              </div>
+
               {this.props.parentState.fileWasUploaded &&
-                <div className="dt-ui-wrapper">
-                  <div className="dt-feedback">
-                    <div className="dt-feedback-content">
-                      <div className="dt-link-group">
-                        <div className="dt-link-label">Swarm Hash</div>
-                        <input type="text" value={this.props.parentState.uploadedFileHash || ""} readOnly={true}/>
-                      </div>
-                    </div>
-                  </div>
+                <div>
+                  <div className="dt-feedback-swarmhash-message">Swarm Hash</div>
+                  <div className="dt-feedback-swarmhash"><input type="text" value={this.props.parentState.uploadedFileHash || ""} readOnly={true}/></div>  
                 </div>
               }
+            </div>
           </div> {/* dt-ui */}
       </div>
     )

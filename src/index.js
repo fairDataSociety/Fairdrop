c62d110922d205dfb77e4e9d3139c5b18f6a4814
significance <dan@1up.digital>
2018-11-19 17:33:30 +0000

    fix to work in bzz:/ subdirectory

--- src/index.js
+++ src/index.js
@@ -5,4 +5,11 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './App';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+
+let bzz = window.location.href.split('/')[window.location.href.split('/').length-2]
+
+let basename = process.env.NODE_ENV === 'production' ? `/bzz:/${bzz}` : `/`;
+
+console.log(process.env.NODE_ENV, basename)
+
+ReactDOM.render(<Router basename={basename}><App /></Router>, document.getElementById('root'));

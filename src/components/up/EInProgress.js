6de23350e3010b7a7c5539f3eb8920add3d529ff
significance <dan@1up.digital>
2019-03-06 17:44:43 +0000

    fixes quick send feedback

--- src/components/up/EInProgress.js
+++ src/components/up/EInProgress.js
@@ -29,18 +29,29 @@ class EInProgress extends Component{
       <div id="in-progress" className={"in-progress green page-wrapper " + (this.props.parentState.uiState === 4 ? "fade-in" : "hidden")}>
           <div className="mist"></div>
           <div className="in-progress-ui page-inner-centered">
-            { !this.props.parentState.fileWasEncrypted &&
-              <div className="in-progress-ui">
-                <h1 className="in-progress-header"><img className="in-progress-icon" src="assets/images/progress.svg" alt="Spinning"/>Encrypting*</h1>
-                <h2 className="in-progress-sub">*AES-256 military grade encryption</h2>
-                <h3 className="in-progress-sub-2">{this.props.parentState.feedbackMessage}</h3>
-              </div>
+            { this.props.parentState.isQuickFile &&
+                <div className="in-progress-ui">
+                  <h1 className="in-progress-header"><img className="in-progress-icon" src="assets/images/progress.svg" alt="Spinning"/>Uploading</h1>
+                  <h2 className="in-progress-sub">Storing Unencrypted in Swarm network</h2>
+                  <h3 className="in-progress-sub-2">{this.props.parentState.feedbackMessage}</h3>
+                </div>  
             }
-            { this.props.parentState.fileWasEncrypted && !this.props.parentState.fileWasUploaded &&
-              <div className="in-progress-ui">
-                <h1 className="in-progress-header"><img className="in-progress-icon" src="assets/images/progress.svg" alt="Spinning"/>Uploading</h1>
-                <h2 className="in-progress-sub">Storing Encrypted in Swarm network</h2>
-                <h3 className="in-progress-sub-2">{this.props.parentState.feedbackMessage}</h3>
+            { !this.props.parentState.isQuickFile &&
+              <div>
+                { !this.props.parentState.encryptionComplete &&
+                  <div className="in-progress-ui">
+                    <h1 className="in-progress-header"><img className="in-progress-icon" src="assets/images/progress.svg" alt="Spinning"/>Encrypting*</h1>
+                    <h2 className="in-progress-sub">*AES-256 military grade encryption</h2>
+                    <h3 className="in-progress-sub-2">{this.props.parentState.feedbackMessage}</h3>
+                  </div>
+                }
+                { this.props.parentState.encryptionComplete && !this.props.parentState.fileWasUploaded &&
+                  <div className="in-progress-ui">
+                    <h1 className="in-progress-header"><img className="in-progress-icon" src="assets/images/progress.svg" alt="Spinning"/>Uploading</h1>
+                    <h2 className="in-progress-sub">Storing Encrypted in Swarm network</h2>
+                    <h3 className="in-progress-sub-2">{this.props.parentState.feedbackMessage}</h3>
+                  </div>
+                }
               </div>
             }
           </div>

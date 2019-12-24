3f5d3d8d2c88d92886841b72d47b2a431f6a6cde
significance <dan@1up.digital>
2019-12-24 19:43:33 +0000

    reverts to use normal fds.js config, and new pinning server

--- src/App.js
+++ src/App.js
@@ -44,7 +44,8 @@ import './lib/DDrop.css';
 import {version} from '../package.json';
 window.files = [];
 
-let pinningOracleURL = 'https://oracle.fairdrop.pro';
+// let pinningOracleURL = 'https://oracle.fairdrop.pro'; //nb this refers to swarm.fairdrop.pro oracle
+let pinningOracleURL = 'https://pinning.fairdrop.pro'; //nb this refers to swarm2.fairdatasociety.org's oracle
 
 class App extends Component {
 
@@ -134,24 +135,24 @@ class App extends Component {
       this.initSentry();    
     }
 
-    let config = {
-      tokenName: 'gas',
-      swarmGateway: 'https://swarm.fairdrop.pro',
-      ethGateway: 'https://noordung.fairdrop.pro',
-      faucetAddress: 'https://faucet-noordung.fairdatasociety.org/gimmie',
-      chainID: '235813',
-      httpTimeout: 1000,
-      gasPrice: 0.1,
-      walletVersion: 1,
-      ensConfig: {
-        domain: 'datafund.eth',
-        registryAddress: '0xA1029cb176082eca658A67fD6807B9bDfB44A695',
-        subdomainRegistrarAddress: '0x0E6a3B5f6800145bAe95C48934B7b5a90Df50722',
-        resolverContractAddress: '0xC91AB84FFad79279D47a715eF91F5fbE86302E4D'
-      }
-    };
+    // let config = {
+    //   tokenName: 'gas',
+    //   swarmGateway: 'https://swarm.fairdatasociety.org',
+    //   ethGateway: 'https://noordung.fairdrop.pro',
+    //   faucetAddress: 'https://faucet-noordung.fairdatasociety.org/gimmie',
+    //   chainID: '235813',
+    //   httpTimeout: 1000,
+    //   gasPrice: 0.1,
+    //   walletVersion: 1,
+    //   ensConfig: {
+    //     domain: 'datafund.eth',
+    //     registryAddress: '0xA1029cb176082eca658A67fD6807B9bDfB44A695',
+    //     subdomainRegistrarAddress: '0x0E6a3B5f6800145bAe95C48934B7b5a90Df50722',
+    //     resolverContractAddress: '0xC91AB84FFad79279D47a715eF91F5fbE86302E4D'
+    //   }
+    // };
 
-    this.FDS = new FDS(config);
+    this.FDS = new FDS();
     window.FDS = this.FDS;
 
     this.uploadComponent = React.createRef();

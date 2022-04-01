da972863c1dab7c3602477af3340ebff6bb1c0b5
Manuel Casas <manuel.casasbarrado@gmail.com>
2022-04-01 18:16:54 +0200

    feat: add file preview component

---
+++ src/components/molecules/filePreview/FilePreview.js
@@ -0,0 +1,68 @@
+// Copyright 2019 The FairDataSociety Authors
+// This file is part of the FairDataSociety library.
+//
+// The FairDataSociety library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The FairDataSociety library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the FairDataSociety library. If not, see <http://www.gnu.org/licenses/>.
+
+import { saturate, lighten } from 'polished'
+import React, { memo, useMemo } from 'react'
+import styled from 'styled-components/macro'
+import { VARIANT } from '../../../theme/theme'
+import { Icon } from '../../atoms/icon/Icon'
+
+const Container = styled.div`
+  width: ${({ isImage }) => (isImage ? '242px' : '200px')};
+  height: ${({ isImage }) => (isImage ? '242px' : '200px')};
+  border-radius: ${({ isImage }) => (isImage ? '4px' : '100px')};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: ${({ theme }) => lighten(0.46, saturate(1, theme?.colors?.[VARIANT.PRIMARY]?.main))};
+  overflow: hidden;
+`
+
+const Preview = styled.img`
+  max-height: 100%;
+  object-fit: cover;
+`
+
+export const FilePreview = memo(({ file, ...props }) => {
+  const iconName = useMemo(() => {
+    const fileType = file?.type
+    if (!fileType) {
+      return 'file'
+    }
+
+    if (fileType.includes('image')) {
+      return 'picture'
+    } else if (fileType.includes('audio')) {
+      return 'music'
+    } else if (fileType.includes('video')) {
+      return 'video'
+    } else {
+      return 'file'
+    }
+  }, [file])
+
+  const isImage = iconName === 'picture'
+
+  return (
+    <Container isImage={isImage} {...props}>
+      {isImage ? <Preview src={file?.source} /> : <Icon size="xl" name={iconName} />}
+    </Container>
+  )
+})
+
+FilePreview.defaultProps = {}
+
+FilePreview.displayName = 'FilePreview'
---
+++ src/components/molecules/filePreview/FilePreview.stories.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { FilePreview } from './FilePreview'
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/FilePreview',
+  component: FilePreview,
+  args: {
+    file: {
+      type: 'image/png',
+      source:
+        'https://images.unsplash.com/photo-1648567451040-f7cd2c60fd35?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1808&q=80',
+    },
+  },
+}
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <FilePreview {...args} />
+
+export const Picture = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Picture.args = {}
+
+export const Audio = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Audio.args = {
+  file: {
+    type: 'audio/mp3',
+  },
+}
+
+export const Video = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Video.args = {
+  file: {
+    type: 'video/mp4',
+  },
+}
+
+export const Document = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Document.args = {
+  file: {
+    type: 'text/plain',
+  },
+}
